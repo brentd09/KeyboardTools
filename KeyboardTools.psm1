2f34a0fb5bc71ac727b70d71dd89c197983c9fc5
BrentD09 <brentdenny@gmail.com>
2018-03-22 11:33:46 +1000

    Updated the prompt
    Show the user that the ESC key exits from the keypress function

--- KeyboardTools.psm1
+++ KeyboardTools.psm1
@@ -1,7 +1,7 @@
 function Get-Keypress {
   if (($host).Name -eq 'ConsoleHost') {
     do {
-      Write-Host "Press any key to continue..."
+      Write-Host "Press any key to continue, [ESC] to Exit..."
       $KeyObj = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
       $KeyObj
     } until ($KeyObj.VirtualKeyCode -eq 27)
